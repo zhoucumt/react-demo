30f1ed44f40b34fb93ff82056d3d3c04a0b57161
zhoucumt <553817478@qq.com>
2020-09-15 00:02:39 +0800

    mapStateToProps用法2

--- src/views/ReactReduxPage/index.js
+++ src/views/ReactReduxPage/index.js
@@ -3,16 +3,27 @@ import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
 
 // mapStateToProps用法1
-const mapStateToProps = ({count}) => ({count});
+// const mapStateToProps = ({count}) => ({count});
+
+// mapStateToProps用法2
+const mapStateToProps = (state) => {
+  return {
+    count: state.count,
+    // 自己定义一个属性
+    num: 2
+  }
+}
+
 
 class ReactReduxPage extends Component {
   render() {
     console.log("props", this.props); //sy-log
-    const {count, dispatch, add, minus} = this.props;
+    const {count, dispatch, add, minus, num} = this.props;
     return (
       <div>
         <h3>ReactReduxPage</h3>
         <p>{count}</p>
+        <p>num: {num}</p>
         <button onClick={() => dispatch({type: "ADD", payload: 100})}>
           dispatch add
         </button>

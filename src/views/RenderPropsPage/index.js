e3d477fa555441d6c05ab9a3a7b9b10c0cfd0007
zhoucumt <553817478@qq.com>
2020-11-22 23:19:55 +0800

    add render props page

--- src/views/RenderPropsPage/index.js
+++ src/views/RenderPropsPage/index.js
@@ -3,11 +3,24 @@ import React, {Component} from 'react';
 class MouseTracker extends Component {
   constructor(props) {
     super(props);
-    this.state = {  };
+    this.state = {
+      x: 0,
+      y: 0
+    };
+  }
+
+  handleMouseMove = (event) => {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY
+    });
   }
   render() {
     return (
-      <div>MouseTracker</div>
+      <div onMouseMove={this.handleMouseMove}>
+        <h1>移动鼠标</h1>
+        <p>你当前的鼠标位置是 {this.state.x}, {this.state.y}</p>
+      </div>
     );
   }
 }

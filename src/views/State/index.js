ca3b496238e60f3a05d115e307891e28ec0aaabc
zhoucumt <553817478@qq.com>
2020-12-20 10:47:47 +0800

    state

--- src/views/State/index.js
+++ src/views/State/index.js
@@ -21,9 +21,22 @@ export default class BatchedDemo extends React.Component {
     this.setState({
       number: this.state.number + 1
     });
+
+    setTimeout(() => {
+      this.setState({
+        number: this.state.number + 4
+      });
+      this.setState({
+        number: this.state.number + 5
+      });
+      this.setState({
+        number: this.state.number + 6
+      });
+    });
   }
 
   render() {
+    console.log('render');
     return <button id="myButton" onClick={this.handleClick}>
       Num: {this.state.number}
     </button>

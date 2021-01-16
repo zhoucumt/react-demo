866f9ff5e9b61f279311b5b75b0e1255278e88a3
zhoucumt <553817478@qq.com>
2021-01-16 09:57:42 +0800

    change

--- src/App.js
+++ src/App.js
@@ -14,3 +14,52 @@ export default function App(props) {
     </div>
   );
 }
+
+/**
+ *
+  const element = <h1 title="foo">Hello</h1>
+  const container = document.getElementById("root")
+  ReactDOM.render(element, container)
+
+  const element = React.createElement(
+    "h1",
+    { title: "foo" },
+    "Hello"
+  )
+  const container = document.getElementById("root")
+  ReactDOM.render(element, container)
+
+  const element = {
+    type: "h1",
+    props: {
+      title: "foo",
+      children: "Hello",
+    },
+  }
+  const container = document.getElementById("root")
+  ReactDOM.render(element, container)
+
+  const element = {
+    type: "h1",
+    props: {
+      title: "foo",
+      children: "Hello",
+    },
+  }​
+  const container = document.getElementById("root")
+  ReactDOM.render(element, container)
+
+  // 
+  const element = {
+    type: "h1",
+    props: {
+      title: "foo",
+      children: "Hello",
+    },
+  }​
+  const container = document.getElementById("root")​
+  const node = document.createElement(element.type)
+  node["title"] = element.props.title​
+  const text = document.createTextNode("")
+  text["nodeValue"] = element.props.children
+ */

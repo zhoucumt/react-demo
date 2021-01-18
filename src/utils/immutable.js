3771cebcca79aadc4bf1f21b3c03a7c158bcc029
zhoucumt <553817478@qq.com>
2021-01-18 23:31:47 +0800

    fromJS

---
+++ src/utils/immutable.js
@@ -0,0 +1,15 @@
+import {fromJS} from 'immutable';
+const immutableState = fromJS ({
+  count: 0
+});
+/**
+ * 打印出来是一种Map数据结构
+ * Map {
+  size: 1,
+  _root: ArrayMapNode { ownerID: OwnerID {}, entries: [ [Array] ] },
+  __ownerID: undefined,
+  __hash: undefined,
+  __altered: false
+}
+ */
+console.log(immutableState);
